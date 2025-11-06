0d4791b5ee0133a7331b725b4c3445c87086d468
Ryan Do <kyogen12622@gmail.com>
2025-11-06 10:55:11 -0500

    todo_list copmlete

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from werkzeug.exceptions import NotFound
 from uuid import uuid4
+from functools import wraps
 from flask import (flash,
                    Flask,
                    render_template,
@@ -8,8 +10,50 @@ from flask import (flash,
                    url_for,
 )
 
+from todos.utils import (complete_todos,
+                         delete_todo_list,
+                         error_for_list_title,
+                         delete_todo_by_id,
+                         error_for_todo,
+                         find_list_by_id,
+                         find_todo_by_id,
+                         is_list_completed,
+                         is_todo_completed,
+                         sort_items,
+                         todos_remaining
+)
+
 app = Flask(__name__)
 app.secret_key = 'secret1'
+
+def require_list(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        list_id = kwargs.get('list_id')
+        lst = find_list_by_id(list_id, session['lists'])
+        if not lst:
+            raise NotFound(description="List not found")
+        return f(lst=lst, *args, **kwargs)
+
+    return decorated_function
+
+def require_todo(f):
+    @wraps(f)
+    @require_list
+    def decorated_function(lst, *args, **kwargs):
+        todo_id = kwargs.get('todo_id')
+        todo = find_todo_by_id(todo_id, lst['todos'])
+        if not todo:
+            raise NotFound(description="Todo not found")
+        return f(lst=lst, todo=todo, *args, **kwargs)
+
+    return decorated_function
+
+@app.context_processor
+def list_utilities_processor():
+    return dict(
+        is_list_completed=is_list_completed,
+    )
 
 @app.before_request
 def initialize_session():
@@ -26,25 +70,103 @@ def add_todo_list():
 
 @app.route("/lists")
 def get_lists():
-    return render_template("lists.html", lists=session["lists"])
+    lists = sort_items(session["lists"], is_list_completed)
+    return render_template("lists.html", lists=lists, todos_remaining=todos_remaining)
+
+@app.route("/lists/<list_id>")
+@require_list
+def show_list(lst, list_id):
+    lst["todos"] = sort_items(lst["todos"], is_todo_completed)
+    return render_template("list.html", lst=lst)
 
 @app.route("/lists", methods=["POST"])
 def create_list():
     title = request.form["list_title"].strip()
 
-    if any(lst["title"] == title for lst in session["lists"]):
-        flash("The title must be unique.", "error")
+    error = error_for_list_title(title, session["lists"])
+    if error:
+        flash(error, "error")
         return render_template("new_list.html", title=title)
 
-    if 1 <= len(title) <= 100:
-        session["lists"].append({"id": str(uuid4()), "title": title, "todos": []})
+    session["lists"].append({"id": str(uuid4()), "title": title, "todos": []})
 
-        flash("The list has been created.", "success")
-        session.modified = True
-        return redirect(url_for("get_lists"))
+    flash("The list has been created.", "success")
+    session.modified = True
+    return redirect(url_for("get_lists"))
 
-    flash("The title must be between 1 and 100 characters.", "error")
-    return render_template("new_list.html", title=title)
+@app.route("/lists/<list_id>/todos", methods=["POST"])
+@require_list
+def add_todo(lst, list_id):
+    todo = request.form["todo"].strip()
+
+    error = error_for_todo(todo)
+    if error:
+        flash(error, "error")
+        return render_template("list.html", lst=lst)
+
+    lst["todos"].append({
+        "id": str(uuid4()),
+        "title": todo,
+        "completed": False
+    })
+
+    flash("The todo was added.", "success")
+    session.modified = True
+    return redirect(url_for("show_list", list_id=list_id))
+
+@app.route("/lists/<list_id>/todos/<todo_id>/toggle", methods=["POST"])
+@require_todo
+def toggle_todo(lst, todo, list_id, todo_id):
+    todo["completed"] = not todo["completed"]
+
+    flash("Todo marked completed.", "success") if todo["completed"] else flash("Todo marked incompleted.", "sucess")
+    session.modified = True
+    return redirect(url_for("show_list", list_id=list_id))
+
+@app.route("/lists/<list_id>/todos/{<todo_id>/delete", methods=["POST"])
+@require_todo
+def delete_todo(lst, todo, list_id, todo_id):
+    delete_todo_by_id(todo_id, lst)
+    flash("Todo deleted.", "success")
+    session.modified = True
+    return redirect(url_for("show_list", list_id=list_id))
+
+@app.route("/lists/<list_id>/complete_all", methods=["POST"])
+@require_list
+def complete_all_todos(lst, list_id):
+    complete_todos(lst)
+    flash("All todos completed.", "success")
+    session.modified = True
+    return redirect(url_for("show_list", list_id=list_id))
+
+@app.route("/lists/<list_id>", methods=["POST"])
+@require_list
+def edit_list_title(lst, list_id):
+    title = request.form["list_title"].strip()
+
+    error = error_for_list_title(title, session["lists"])
+    if error:
+        flash(error, "error")
+        return render_template("edit_list.html", lst=lst, title=title)
+
+    lst["title"] = title
+
+    flash("The list title was updated.", "success")
+    session.modified = True
+    return redirect(url_for("show_list", list_id=list_id))
+
+@app.route("/lists/<list_id>/edit")
+@require_list
+def edit_list(lst, list_id):
+    return render_template("edit_list.html", lst=lst)
+
+@app.route("/lists/<list_id>/delete", methods=["POST"])
+@require_list
+def delete_list(lst, list_id):
+    delete_todo_list(list_id, session)
+    flash("Todo list deleted.", "success")
+    session.modified = True
+    return redirect(url_for("get_lists"))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
---
+++ todos/utils.py
@@ -0,0 +1,54 @@
+def error_for_list_title(title, lists):
+    if any(lst['title'] == title for lst in lists):
+        return "The title must be unique."
+    elif not 1 <= len(title) <= 100:
+        return "The title must be between 1 and 100 characters"
+    else:
+        return None
+
+def find_list_by_id(list_id, lists):
+    return next((lst for lst in lists if lst["id"] == list_id), None)
+
+def find_todo_by_id(todo_id, list):
+    return next((todo for todo in list if todo["id"] == todo_id), None)
+
+def error_for_todo(todo):
+    if len(todo) == 0:
+        return "Title is required."
+    elif len(todo) > 100:
+        return "The title is too long. Must be 100 characters or less."
+    else:
+        return None
+
+def delete_todo_by_id(todo_id, lst):
+    lst['todos'] = [todo for todo in lst['todos'] if todo['id'] != todo_id]
+    return None
+
+def complete_todos(lst):
+    for todo in lst["todos"]:
+        if not todo["completed"]:
+            todo["completed"] = True
+    return None
+
+def delete_todo_list(list_id, session):
+    session["lists"] = [lst for lst in session["lists"] if lst["id"] != list_id]
+    return None
+
+def todos_remaining(lst):
+    return sum(1 for todo in lst["todos"] if not todo["completed"])
+
+def is_list_completed(lst):
+    return len(lst["todos"]) > 0 and todos_remaining(lst) == 0
+
+def is_todo_completed(todo):
+    return todo['completed']
+
+def sort_items(items, select_completed):
+    sorted_items = sorted(items, key=lambda item: item['title'].lower())
+
+    incomplete_items = [item for item in sorted_items
+                        if not select_completed(item)]
+    complete_items = [item for item in sorted_items
+                      if select_completed(item)]
+
+    return incomplete_items + complete_items
